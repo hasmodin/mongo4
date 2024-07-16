9f51894140761ad7456afbb660273f493b6d9b59
Hasmodin Ansari <hasmodin@gmail.com>
2024-07-16 18:15:33 +0300

    updated

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const employee = require("./models/db");
+const path = require("path");
+const methodOverride = require("method-override");
 const app = express();
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public/css")));
+app.use(express.static(path.join(__dirname, "public/js")));
+
+app.use(express.urlencoded({extended :true}));
+
+app.use(methodOverride('_method'));
+
 
 main()
 .then((result) => {
@@ -37,6 +51,51 @@ async function main() {
 
 app.get("/", (req, res) => {
     res.send("root is working");
+});
+
+app.get("/employees", async (req, res) => {
+    const allEmployees =  await employee.find();
+   
+    res.render("index.ejs", {allEmployees});
+});
+
+app.get("/employees/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/employees", (req, res) => {
+    let {name, email, designation, salary} = req.body;
+    let updatedEmployee = new employee({
+        name: name,
+        email: email,
+        designation: designation,
+        salary: salary,
+        joining_date: new Date(),
+    })
+    updatedEmployee.save()
+    console.log(updatedEmployee);
+    res.redirect("/employees");
+});
+
+app.get("/employees/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    let employee1 = await employee.findById(id);
+    res.render("edit.ejs", {employee1});
+});
+
+app.put("/employees/:id", async (req, res) => {
+    let {id} = req.params;
+    let {email: newEmail} = req.body;
+    let updatedEmployee = await employee.findByIdAndUpdate(id, {email: newEmail});
+   
+    res.redirect("/employees");
+    
+})
+
+app.delete("/employees/:id", async (req, res) => {
+    let {id} = req.params;
+    let deleteEmployee = await employee.findByIdAndDelete(id);
+    res.redirect("/employees");
 })
 
 app.listen(8080, () => {
---
+++ public/js/app.js
@@ -0,0 +1,42 @@
+// const deleteBtn = document.getElementById("deletebtn");
+
+// deleteBtn.addEventListener("click", () => {
+//     console.log("button was clicked");
+//     function confirmDelete() {
+//         const result = confirm("Are you sure you want to delete");
+//         if(result) {
+//             deleteItem();
+//         }else {
+//             return false;
+//         }
+//     }
+// });
+
+
+// function deleteItem() {
+//     const item = document.querySelector(".employee");
+//     item.remove();
+// }
+
+// let h1 = document.createElement("h1");
+// h1.innerHTML = "this is from js";
+// document.querySelector("body").append(h1);
+// const mongoose = require("mongoose");
+// const employee = require("./models/db");
+
+// const btn = document.getElementById("deletebtn");
+
+// btn.addEventListener("click", () => {
+//     let result = confirm("Are you sure you want to delete");
+//     if(result) {
+//         deleteItem();
+//     }else {
+//         return false;
+//     }
+// })
+
+// async function  deleteItem() {
+//     let {id} = req.params;
+//     let allemployees =  await employee.findByIdAndDelete(id);
+
+// }
